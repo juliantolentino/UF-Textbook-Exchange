4395e15438d09777c118c10844142fd1d5e434a3
juliantolentino <julianntolentino@gmail.com>
2017-10-29 22:08:33 -0400

    transferred work from old repo - the toolbar and homepage frontend

---
+++ src/app/ui/filter-page/filter-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'filter-page',
+  templateUrl: './filter-page.component.html',
+  styleUrls: ['./filter-page.component.scss']
+})
+export class FilterPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
